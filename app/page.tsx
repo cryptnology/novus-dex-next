efba3873113fc0cb7fe93c5a4504d7796844d2c4
cryptnology <jamie@cryptnology.dev>
2023-05-07 15:12:56 +0100

    Add transition

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Container } from '@/components';
 const Home = () => {
   return (
     <Container className="">
-      <main className="pt-32 text-dark dark:text-light">
+      <main className="pt-32 text-dark dark:text-light transition">
         <h1>Novus Dex</h1>
       </main>
     </Container>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,15 +4,15 @@ const NavBar = () => {
   return (
     <Container className="w-full py-5 flex items-center justify-between bg-light dark:bg-dark z-10 text-dark dark:text-light fixed top-0 left-0">
       <div className="bg-primary h-16 w-16 flex items-center justify-center rounded-full dark:bg-primaryDark cursor-default">
-        <div className="bg-light h-[3.7rem] w-[3.7rem] rounded-full flex items-center justify-center text-sm font-semibold text-primary dark:text-primaryDark dark:bg-dark">
+        <div className="bg-light h-[3.7rem] w-[3.7rem] rounded-full flex items-center justify-center text-sm font-semibold text-primary dark:text-primaryDark dark:bg-dark transition">
           Novus
         </div>
       </div>
       <div className="flex items-center">
-        <button className="mr-6 px-6 py-2 text-light bg-primary rounded-xl hover:bg-light hover:text-dark border-[3px] border-transparent hover:border-primary transition-opacity dark:bg-primaryDark dark:text-dark dark:hover:text-light dark:hover:border-primaryDark dark:hover:border-[3px] dark:hover:bg-dark">
+        <button className="mr-6 px-6 py-2 text-light font-bold bg-primary rounded-xl hover:bg-light hover:text-dark border-[3px] border-transparent hover:border-primary dark:bg-primaryDark dark:text-dark dark:hover:text-light dark:hover:border-primaryDark dark:hover:border-[3px] dark:hover:bg-dark transition duration-300">
           Connect
         </button>
-        <ToggleThemeButton className="flex items-center justify-center rounded-full p-1 bg-primary dark:bg-primaryDark text-light dark:text-dark w-[1.65rem] h-[1.65rem]" />
+        <ToggleThemeButton className="flex items-center justify-center rounded-full p-1 bg-primary dark:bg-primaryDark text-light dark:text-dark w-[1.65rem] h-[1.65rem] transition" />
       </div>
     </Container>
   );
