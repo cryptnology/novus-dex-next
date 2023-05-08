3bccf909c0eb989ae52492f992feabd2ac7f787b
cryptnology <jamie@cryptnology.dev>
2023-05-08 09:50:43 +0100

    Add balance to NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { Container, ToggleThemeButton } from '@/components';
+import { Wallet } from '@/icons';
 
 const NavBar = () => {
   return (
@@ -9,10 +10,18 @@ const NavBar = () => {
         </div>
       </div>
       <div className="flex items-center">
-        <button className="mr-6 px-6 py-2 text-light font-bold bg-primary rounded-xl hover:bg-light hover:text-dark border-[3px] border-transparent hover:border-primary dark:bg-primaryDark dark:text-dark dark:hover:text-light dark:hover:border-primaryDark dark:hover:border-[3px] dark:hover:bg-dark transition duration-300">
-          Connect
-        </button>
-        <ToggleThemeButton className="flex items-center justify-center rounded-full p-1 bg-primary dark:bg-primaryDark text-light dark:text-dark w-[1.65rem] h-[1.65rem] transition" />
+        <div className="bg-primary/40 dark:bg-primaryDark/40 flex items-center rounded-xl transition">
+          <div className="px-6 text-dark dark:text-light flex items-center">
+            <Wallet className="w-6" />
+            <span className="ml-3 text-sm">
+              100<span className="ml-1 text-base">ETH</span>
+            </span>
+          </div>
+          <button className="px-6 py-2 text-light font-bold bg-primary rounded-xl hover:bg-light hover:text-dark border-[3px] border-transparent hover:border-primary dark:bg-primaryDark dark:text-dark dark:hover:text-light dark:hover:border-primaryDark dark:hover:border-[3px] dark:hover:bg-dark transition duration-300">
+            Connect
+          </button>
+        </div>
+        <ToggleThemeButton className="ml-6 flex items-center justify-center rounded-full p-1 bg-primary dark:bg-primaryDark text-light dark:text-dark w-[1.65rem] h-[1.65rem] transition" />
       </div>
     </Container>
   );
