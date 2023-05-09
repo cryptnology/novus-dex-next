cc52f24adc5ff01a51a846fd5bbe74987058c637
cryptnology <jamie@cryptnology.dev>
2023-05-09 08:58:17 +0100

    Add blockies to NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import { useEffect } from 'react';
+import Blockies from 'react-blockies';
 import { Container, ToggleThemeButton } from '@/components';
 import { Wallet } from '@/icons';
 import { useBlockchainStore } from '@/store';
 import { ethers } from 'ethers';
-import { useEffect } from 'react';
+
+import config from '../config.json';
 
 const NavBar = () => {
   const {
@@ -54,17 +57,45 @@ const NavBar = () => {
         </div>
       </div>
       <div className="flex items-center">
-        <div className="bg-primary/40 dark:bg-primaryDark/40 flex items-center rounded-xl transition">
-          <div className="px-6 text-dark dark:text-light flex items-center">
+        <div
+          className={`bg-primary/40 dark:bg-primaryDark/40 h-[46px] ${
+            account ? 'px-6' : 'pl-6'
+          } flex items-center rounded-xl transition`}
+        >
+          <div className="pr-4 text-dark dark:text-light flex items-center">
             <Wallet className="w-6" />
             <span className="ml-3 text-sm">
               {balance ? Number(balance).toFixed(4) : '0'}
               <span className="ml-1 text-base">ETH</span>
             </span>
           </div>
-          <button className="px-6 py-2 text-light font-bold bg-primary rounded-xl hover:bg-light hover:text-dark border-[3px] border-transparent hover:border-primary dark:bg-primaryDark dark:text-dark dark:hover:text-light dark:hover:border-primaryDark dark:hover:border-[3px] dark:hover:bg-dark transition duration-300">
-            Connect
-          </button>
+          {account ? (
+            <a
+              className="flex items-center text-sm"
+              href={
+                // @ts-ignore
+                config[chainId]
+                  ? // @ts-ignore
+                    `${config[chainId].explorerURL}/address/${account}`
+                  : `#`
+              }
+            >
+              {account.slice(0, 5) + '...' + account.slice(38, 42)}
+              <Blockies
+                seed={account}
+                size={10}
+                scale={2.5}
+                color="#2187D0"
+                bgColor="#F1F2F9"
+                spotColor="#767F92"
+                className="ml-4 rounded-full"
+              />
+            </a>
+          ) : (
+            <button className="px-6 py-2 text-light font-bold bg-primary rounded-xl hover:bg-light hover:text-dark border-[3px] border-transparent hover:border-primary dark:bg-primaryDark dark:text-dark dark:hover:text-light dark:hover:border-primaryDark dark:hover:border-[3px] dark:hover:bg-dark transition duration-300">
+              Connect
+            </button>
+          )}
         </div>
         <ToggleThemeButton className="ml-6 flex items-center justify-center rounded-full p-1 bg-primary dark:bg-primaryDark text-light dark:text-dark w-[1.65rem] h-[1.65rem] transition" />
       </div>
