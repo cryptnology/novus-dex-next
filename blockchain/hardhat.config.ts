4b7decff43b69eaf37641372d579e6e1a5281fe0
cryptnology <jamie@cryptnology.dev>
2023-05-11 08:40:43 +0100

    Remove import

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,8 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
+// import { HardhatUserConfig } from 'hardhat/config';
+import "@nomicfoundation/hardhat-toolbox";
 
-const config: HardhatUserConfig = {
-  solidity: '0.8.9',
+const config = {
+  solidity: "0.8.9",
   networks: {
     localhost: {},
   },
